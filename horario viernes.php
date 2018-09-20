883b39311975ecf0f6933d8d879752a322a070c4
davidcordoba99 <43339357+davidcordoba99@users.noreply.github.com>
2018-09-20 13:57:00 +0200

    Add files via upload

---
+++ horario viernes.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$Hora = date("H");
+$Dia = date("N");
+$Hora = $Hora += 2;
+
+if ($Dia == "4") {
+if ($Hora <= "8". "<br>") {
+    echo "Redes";
+} elseif ($Hora <= "9". "<br>") {
+    echo "Redes";
+} elseif ($Hora <= "10". "<br>") {
+    echo "Sistemas";
+} elseif ($Hora <= "11". "<br>") {
+    echo "Sistemas";
+} elseif ($Hora <= "12". "<br>") {
+    echo "Recreo";
+} elseif ($Hora <= "13". "<br>") {
+    echo "App Web";
+} elseif ($Hora <= "14". "<br>") {
+    echo "App Web";
+}
+}
+if ($Dia == "1") {
+    if ($Hora <= "8". "<br>") {
+        echo "Bases de datos";
+    } elseif ($Hora <= "9". "<br>") {
+        echo "EIE";
+    } elseif ($Hora <= "10". "<br>") {
+        echo "Sistemas";
+    } elseif ($Hora <= "11". "<br>") {
+        echo "Sistemas";
+    } elseif ($Hora <= "12". "<br>") {
+        echo "Recreo";
+    } elseif ($Hora <= "13". "<br>") {
+        echo "App Web";
+    } elseif ($Hora <= "14". "<br>") {
+        echo "App Web";
+    }
+    }
+    if ($Dia == "3") {
+        if ($Hora <= "8". "<br>") {
+            echo "Seguridad";
+        } elseif ($Hora <= "9". "<br>") {
+            echo "Seguridad";
+        } elseif ($Hora <= "10". "<br>") {
+            echo "Sistemas";
+        } elseif ($Hora <= "11". "<br>") {
+            echo "Sistemas";
+        } elseif ($Hora <= "12". "<br>") {
+            echo "Recreo";
+        } elseif ($Hora <= "13". "<br>") {
+            echo "EIE";
+        } elseif ($Hora <= "14". "<br>") {
+            echo "EIE";
+        }
+        }
+        if ($Dia == "2") {
+            if ($Hora <= "8". "<br>") {
+                echo "App Web";
+            } elseif ($Hora <= "9". "<br>") {
+                echo "Seguridad";
+            } elseif ($Hora <= "10". "<br>") {
+                echo "Redes";
+            } elseif ($Hora <= "11". "<br>") {
+                echo "Redes";
+            } elseif ($Hora <= "12". "<br>") {
+                echo "Recreo";
+            } elseif ($Hora <= "13". "<br>") {
+                echo "Bases de datos";
+            } elseif ($Hora <= "14". "<br>") {
+                echo "Bases de datos";
+            }
+            }
+            if ($Dia == "5") {
+                if ($Hora <= "8". "<br>") {
+                    echo "Seguridad";
+                } elseif ($Hora <= "9". "<br>") {
+                    echo "Seguridad";
+                } elseif ($Hora <= "10". "<br>") {
+                    echo "Redes";
+                } elseif ($Hora <= "11". "<br>") {
+                    echo "Recreo";
+                } elseif ($Hora <= "12". "<br>") {
+                    echo "Redes";
+                } elseif ($Hora <= "13". "<br>") {
+                    echo "Ingles";
+                } elseif ($Hora <= "14". "<br>") {
+                    echo "Ingles";
+                }
+                }
+?>
+ 
+</body>
+</html>
